2b2c25f70ba956a01950a1dd21f1025a95eb2758
CuckooDuo <147583687+CuckooDuo@users.noreply.github.com>
2024-10-24 15:26:43 +0800

    Update 6d_BFSfailure.py

--- math/6d_BFSfailure.py
+++ math/6d_BFSfailure.py
@@ -1,13 +1,13 @@
 import subprocess
 import csv
 
-def run_4d():
-    compile_command = ["g++", "4d_BFSfailure.cpp", "-o", "4d_BFSfailure"]
+def run_6d():
+    compile_command = ["g++", "6d_BFSfailure.cpp", "-o", "6d_BFSfailure"]
     subprocess.run(compile_command, check=True)
     
-    result = subprocess.run(["./4d_BFSfailure"], capture_output=True, text=True).stdout.strip()
+    result = subprocess.run(["./6d_BFSfailure"], capture_output=True, text=True).stdout.strip()
     
     return result
 
-run_4d()
-print("Results saved to 4d.csv")
+run_6d()
+print("Results saved to 6d.csv")
